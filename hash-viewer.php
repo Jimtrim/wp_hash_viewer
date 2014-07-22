d2dbdec07f4ccda7c860d48577445d440fbe1b02
Jim Frode Hoff <jimfrodehoff@gmail.com>
2014-07-22 10:02:43 +0200

    add initial files

---
+++ hash-viewer.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: InstagramHashViewer
+ * Plugin URI: http://www.hashviewer.net/
+ * Description: View and rate Instagram pictures
+ * Version: 1.0
+ * Author: Jim Frode Hoff
+ * Author URI: http://jimtrim.github.io
+ */
+
+
