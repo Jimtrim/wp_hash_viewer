595c16a53bc01027339ec90d7d0cc70ab3b2839c
Jim Frode Hoff <jimfrodehoff@gmail.com>
2014-07-23 11:20:08 +0200

    link fix

--- views/admin.php
+++ views/admin.php
@@ -1,6 +1,6 @@
 <?php 
 wp_enqueue_style( 'bootstrap_style', plugins_url( 'hash-viewer/css/bootstrap.min.css'));
-wp_enqueue_style( 'hashviewer_gallery', plugins_url( 'hash-viewer/css/gallery.css'));
+wp_enqueue_style( 'hashviewer_style', plugins_url( 'hash-viewer/css/main.css'));
 wp_enqueue_script( 'hashviewer_script', plugins_url( 'hash-viewer/js/main.js' ));
 ?>
 
