669978ba393deb1a3fde8135c1868e8f7e51582a
Jim Frode Hoff <jimfrodehoff@gmail.com>
2014-07-25 14:35:26 +0200

    tidy up with TidyPHP and Javascript Beauitfy

--- classes/HashViewer.class.php
+++ classes/HashViewer.class.php
@@ -14,17 +14,17 @@ class HashViewer {
 
 	private function __construct() {
 
-		# Load Composer plugins
-		require_once(HASHVIEWER_PLUGIN_DIR. '/vendor/autoload.php');
-		# Load Instagram-PHP-API
-		require_once(HASHVIEWER_PLUGIN_DIR. '/vendor/Instagram.class.php');
+		// Load Composer plugins
+		require_once HASHVIEWER_PLUGIN_DIR. '/vendor/autoload.php';
+		// Load Instagram-PHP-API
+		require_once HASHVIEWER_PLUGIN_DIR. '/vendor/Instagram.class.php';
 
-		$loader = new Twig_Loader_Filesystem(HASHVIEWER_PLUGIN_DIR . '/views/');
-		$this->twig = new Twig_Environment($loader);
+		$loader = new Twig_Loader_Filesystem( HASHVIEWER_PLUGIN_DIR . '/views/' );
+		$this->twig = new Twig_Environment( $loader );
 
 		// Add default actions
-		add_action( 'admin_init', array($this, 'admin_init') );
-		add_action( 'admin_menu', array($this, 'admin_menu_setup'));
+		add_action( 'admin_init', array( $this, 'admin_init' ) );
+		add_action( 'admin_menu', array( $this, 'admin_menu_setup' ) );
 		add_action( 'wp_enqueue_styles', array( $this, 'register_frontend_styles' ) );
 		add_action( 'wp_enqueue_scripts', array( $this, 'register_frontend_scripts' ) );
 
@@ -49,18 +49,18 @@ class HashViewer {
 
 	// Admin
 	public function admin_menu_setup() {
-		add_menu_page( "HashViewer", "HashViewer", 'manage_options', 
-			$this->slugs['main'], array($this, 'all_competitions'), HASHVIEWER_PLUGIN_URL . '/img/menu_icon.png' );
+		add_menu_page( "HashViewer", "HashViewer", 'manage_options',
+			$this->slugs['main'], array( $this, 'all_competitions' ), HASHVIEWER_PLUGIN_URL . '/img/menu_icon.png' );
 		add_submenu_page( $this->slugs['main'], "HashViewer - Browse", "All Competitions", 'manage_options',
-			$this->slugs['main']); // main menu item
+			$this->slugs['main'] ); // main menu item
 		add_submenu_page( "hashviewer_main", "HashViewer - New competition", "New competition", 'manage_options',
-			$this->slugs['new_competition'], array($this, 'new_competition') );
+			$this->slugs['new_competition'], array( $this, 'new_competition' ) );
 		add_submenu_page( "hashviewer_main", "HashViewer - Browse", "Browse Instagram", 'manage_options',
-			$this->slugs['browse'], array($this, 'browse') );
+			$this->slugs['browse'], array( $this, 'browse' ) );
 	}
-	
+
 	public function admin_init() {
-		wp_enqueue_script( 'hashviewer_script', HASHVIEWER_PLUGIN_URL . 'js/main.js', array('jquery') );
+		wp_enqueue_script( 'hashviewer_script', HASHVIEWER_PLUGIN_URL . 'js/main.js', array( 'jquery' ) );
 
 
 		wp_register_style( 'bootstrap-style', HASHVIEWER_PLUGIN_URL . 'css/bootstrap.min.css' );
@@ -73,102 +73,102 @@ class HashViewer {
 
 	/**
 	 * Views
-	 */
+	 * */
 	public function all_competitions() {
-		if ( $_SERVER["REQUEST_METHOD"] == "POST" ){
-			if ($_POST["action"] == "delete-competition" && isset($_POST["compId"])){
-				$this->deleteCompetition($_POST["compId"]);
-				echo $this->twig->render('competition_action.twig.html', array(
-					"action"		=> "deleted",
-					"return_url" => get_admin_url() . 'admin.php?page=' . $this->slugs['main']
-				));
+		if ( $_SERVER["REQUEST_METHOD"] == "POST" ) { //POST
+			if ( $_POST["action"] == "delete-competition" && isset( $_POST["compId"] ) ) {
+				$this->deleteCompetition( $_POST["compId"] );
+				echo $this->twig->render( 'competition_action.twig.html', array(
+						"action"  => "deleted",
+						"return_url" => get_admin_url() . 'admin.php?page=' . $this->slugs['main']
+					) );
 			}
-		} else {
+		} else { //GET
 			$data = array(
-				"plugin_url"	=> HASHVIEWER_PLUGIN_URL,
-				"new_comp_url"	=> get_admin_url() . 'admin.php?page=' . $this->slugs['new_competition'], 
-				"competitions" 	=> $this->getAllCompetitions(),
-				"browse_url"	=> get_admin_url() . 'admin.php?page=' . $this->slugs['browse'] 
+				"plugin_url" => HASHVIEWER_PLUGIN_URL,
+				"new_comp_url" => get_admin_url() . 'admin.php?page=' . $this->slugs['new_competition'],
+				"competitions"  => $this->getAllCompetitions(),
+				"browse_url" => get_admin_url() . 'admin.php?page=' . $this->slugs['browse']
 			);
-			echo $this->twig->render('all_competitions.twig.html', $data);
+			echo $this->twig->render( 'all_competitions.twig.html', $data );
 		}
 	}
 	public function new_competition() {
 
-		if ( $_SERVER["REQUEST_METHOD"] == "POST" ){
-			if ($_POST["action"] == "create-competition"){
+		if ( $_SERVER["REQUEST_METHOD"] == "POST" ) { //POST
+			if ( $_POST["action"] == "create-competition" ) {
 				$this->createNewCompetition();
 				$return_url = "";
-				echo $this->twig->render('competition_action.twig.html', array(
-					"action"		=> "created",
-					"return_url"	=> get_admin_url() . 'admin.php?page=' . $this->slugs['main']
-				));
+				echo $this->twig->render( 'competition_action.twig.html', array(
+						"action"  => "created",
+						"return_url" => get_admin_url() . 'admin.php?page=' . $this->slugs['main']
+					) );
 			}
-		} else {
+		} else { //GET
 			$data = array(
-				"plugin_url"	=> HASHVIEWER_PLUGIN_URL,
-				"admin_url"		=> get_admin_url(),
-				"request_url"	=> $_SERVER['REQUEST_URI'],
-				"all_comps_url"	=> get_admin_url() . 'admin.php?page=' . $this->slugs['main']
+				"plugin_url" => HASHVIEWER_PLUGIN_URL,
+				"admin_url"  => get_admin_url(),
+				"request_url" => $_SERVER['REQUEST_URI'],
+				"all_comps_url" => get_admin_url() . 'admin.php?page=' . $this->slugs['main']
 			);
-			echo $this->twig->render('new_competition.twig.html', $data);
+			echo $this->twig->render( 'new_competition.twig.html', $data );
 		}
 	}
 
 
 	public function browse() {
-		
+
 		$data = array();
-		if (isset($_GET['compId'])) {
-			$data['comp'] = $this->getCompetition($_GET['compId']); //TODO: sanitize input
+		if ( isset( $_GET['compId'] ) ) {
+			$data['comp'] = $this->getCompetition( $_GET['compId'] ); //TODO: sanitize input
 		}
 
-		echo $this->twig->render('browse.twig.html', $data);
+		echo $this->twig->render( 'browse.twig.html', $data );
 	}
 
 
 	/**
 	 * Installation functions
-	 */
+	 * */
 	public function plugin_activate() {
-		if (get_option( "ihw_db_version", "Missing" ) == "Missing") {
+		if ( get_option( "ihw_db_version", "Missing" ) == "Missing" ) {
 			$this->db_install();
-			add_option( "ihw_db_version", "0.2");
+			add_option( "ihw_db_version", "0.2" );
 		}
 	}
 
 	public function plugin_deactivate() {
-		delete_option("ihw_db_version" );
+		delete_option( "ihw_db_version" );
 	}
 	public function plugin_uninstall() {
-		if ( !defined( 'WP_UNINSTALL_PLUGIN' ) ) 
-    		exit();
-    	$this->db_uninstall();
+		if ( !defined( 'WP_UNINSTALL_PLUGIN' ) )
+			exit();
+		$this->db_uninstall();
 	}
 	/**
-	 * DB functions 
-	 */
-	public function getCompetition($id) {
+	 * DB functions
+	 * */
+	public function getCompetition( $id ) {
 		global $wpdb;
-		$table_name = $wpdb->prefix . "hashviewer_competition";	
-		$sql = "SELECT active, title, startTime, endTime, hashtags, winnerSubmissionId 
+		$table_name = $wpdb->prefix . "hashviewer_competition";
+		$sql = "SELECT active, title, startTime, endTime, hashtags, winnerSubmissionId
 				FROM $table_name
 				WHERE id='$id';";
 		$rows = $wpdb->get_results( $sql );
 
-		if ( isset($rows[0]) ){
+		if ( isset( $rows[0] ) ) {
 			return $rows[0];
 		} else {
 			return NULL;
 		}
 	}
 
-	public function deleteCompetition($id) {
+	public function deleteCompetition( $id ) {
 		global $wpdb;
-		$table_name = $wpdb->prefix . "hashviewer_competition";	
-		$rows = $wpdb->delete( $table_name , array( 'id' => $id ));
+		$table_name = $wpdb->prefix . "hashviewer_competition";
+		$rows = $wpdb->delete( $table_name , array( 'id' => $id ) );
 
-		if ( isset($rows[0]) ){
+		if ( isset( $rows[0] ) ) {
 			return $rows[0];
 		} else {
 			return NULL;
@@ -177,8 +177,8 @@ class HashViewer {
 
 	public function getAllCompetitions() {
 		global $wpdb;
-		$table_name = $wpdb->prefix . "hashviewer_competition";	
-		$sql = "SELECT id, active, title, startTime, endTime, hashtags, winnerSubmissionId 
+		$table_name = $wpdb->prefix . "hashviewer_competition";
+		$sql = "SELECT id, active, title, startTime, endTime, hashtags, winnerSubmissionId
 				FROM $table_name;";
 
 		$rows = $wpdb->get_results( $sql );
@@ -187,26 +187,26 @@ class HashViewer {
 
 	public static function createNewCompetition() {
 		// TODO: sanitize input
-		$title = (isset($_POST['title'])) ? $_POST['title'] : "" ;
-		$hashtags = (isset($_POST['hashtags'])) ? $_POST['hashtags'] : "" ;
-		$startTime = (isset($_POST['startDay'])) ? $_POST['startDay'] : "" ;
-		$endTime = (isset($_POST['endDay'])) ? $_POST['endDay'] : "" ;
+		$title = ( isset( $_POST['title'] ) ) ? $_POST['title'] : "" ;
+		$hashtags = ( isset( $_POST['hashtags'] ) ) ? $_POST['hashtags'] : "" ;
+		$startTime = ( isset( $_POST['startDay'] ) ) ? $_POST['startDay'] : "" ;
+		$endTime = ( isset( $_POST['endDay'] ) ) ? $_POST['endDay'] : "" ;
 
 
 
 		global $wpdb;
-		$table_name = $wpdb->prefix . "hashviewer_competition";	
+		$table_name = $wpdb->prefix . "hashviewer_competition";
 
 		//$startTime = strtotime($startTime);
 		//$endTime = strtotime($endTime);
 
 
-		$competition = array( 
-			'title'		=> $title, 
-			'hashtags' 	=> $hashtags, 
-			'startTime'	=> $startTime . " 00:00:00",
-			'endTime' 	=> $endTime . " 23:59:59",
-			'active' 	=> 0
+		$competition = array(
+			'title'  => $title,
+			'hashtags'  => $hashtags,
+			'startTime' => $startTime . " 00:00:00",
+			'endTime'  => $endTime . " 23:59:59",
+			'active'  => 0
 		);
 		$affected_rows = $wpdb->insert( $table_name, $competition );
 		return $affected_rows;
@@ -216,7 +216,7 @@ class HashViewer {
 		global $wpdb;
 
 		// come back to me if having \only\ 9 999 999 competitions is a problem
-		$competition_table_name = $wpdb->prefix . "hashviewer_competition";		
+		$competition_table_name = $wpdb->prefix . "hashviewer_competition";
 		$competition_sql = "CREATE TABLE " . $competition_table_name . "(
 			id 					mediumint(9) NOT NULL AUTO_INCREMENT,
 			title 				VARCHAR(50) NOT NULL,
@@ -238,20 +238,34 @@ class HashViewer {
 			tags 				VARCHAR(255),
 			caption 			TEXT,
 			approved 			Bool,
-			createdAt 			DATETIME, -- The time when the image was uploaded to Instagram 
+			createdAt 			DATETIME, -- The time when the image was uploaded to Instagram
 			PRIMARY KEY (id)
 		) CHARACTER SET utf8 COLLATE utf8_unicode_ci;";
 
-		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+		require_once ABSPATH . 'wp-admin/includes/upgrade.php';
 		dbDelta( $submission_sql );
 		dbDelta( $competition_sql );
 	}
 
 	/**
 	 * Utilities
-	 */
-	public function filter_hashtag($tag) {
+	 * */
+	public function filter_hashtag( $tag ) {
 
-		return preg_match("\W*\w*", $tag);
+		return preg_match( "\W*\w*", $tag );
 	}
+
+
+
+	/**
+	 *  Test area
+	 * */
+
+
+
+
+
+
+
+
 }
--- hash-viewer.php
+++ hash-viewer.php
@@ -8,20 +8,40 @@
  * Author URI: http://jimtrim.github.io
  */
 
-// Instagram client_id = 
+// Instagram client_id =
 if ( ! defined( 'ABSPATH' ) ) exit;
 
 define( 'HASHVIEWER_VERSION', '3.0.1' );
 define( 'HASHVIEWER_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'HASHVIEWER_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 
-require_once(plugin_dir_path( __FILE__ ) . 'classes/HashViewer.class.php');
+require_once plugin_dir_path( __FILE__ ) . 'classes/HashViewer.class.php';
 
 register_activation_hook( __FILE__, array( 'HashViewer', 'plugin_activate' ) );
 register_deactivation_hook( __FILE__, array( 'HashViewer', 'plugin_deactive' ) );
-register_uninstall_hook(__FILE__, array( 'HashViewer', 'plugin_uninstall' ));
+register_uninstall_hook( __FILE__, array( 'HashViewer', 'plugin_uninstall' ) );
 
 $viewer = HashViewer::get_instance();
 
 
+add_action( 'admin_footer', array( $viewer, 'test_ajax' ) );
 
+
+function my_action_javascript() {
+?>
+<script type="text/javascript" >
+jQuery(document).ready(function($) {
+
+	var data = {
+		'action': 'my_action',
+		'whatever': 1234
+	};
+
+	// since 2.8 ajaxurl is always defined in the admin header and points to admin-ajax.php
+	$.post(ajaxurl, data, function(response) {
+		console.log('Got this from the server: ' + response);
+	});
+});
+</script>
+<?php
+}
